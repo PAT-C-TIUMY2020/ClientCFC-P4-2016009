70378f402aad9a5308f9950cff29672e1db945bd
Arrasy <arrasyjunior@gmail.com>
2020-10-23 17:40:20 +0700

    Add project files.

---
+++ ClientCFC-P4-2016009/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using ServiceMtk_P2_20160140009;
+
+namespace ClientCFC_P4_2016009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+
+            IMatematika obj = objChannel.CreateChannel();
+
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
